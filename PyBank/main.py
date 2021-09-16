a51871b0d20730f47faa3273a3b057b90f952508
sweeneygood <sweeneygood@gmail.com>
2021-09-15 21:17:22 -0500

    Fixed issue with greatest inc/dec calc

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,17 +2,23 @@ import os
 import csv
 
 months = []
-budgetPerMonth = []
-totalMonths = 0 
-totalAmt = 0 
-PLAmt = []
-previousAmt = 0
-changeAmt = []
-averageChange = 0
+budget_per_month = []
+
+
+total_months = 0 
+total_amt = 0 
+
+average_change = 0
+
+previous_amt = 0
+change_amt = 0
+greatest_increase_amt = 0
+greatest_decrease_amt = 0
+greatest_increase_month = 0
+greatest_decrease_month = 0
 
 # Read in the cvs and add values to lists
 budget_csv = os.path.join("..", "Resources", "budget_data.csv")
-
 
 with open(budget_csv,encoding='utf-8-sig') as csvfile:
 
@@ -24,34 +30,46 @@ with open(budget_csv,encoding='utf-8-sig') as csvfile:
 
     # Read each row of data after the header
     # loop through the file to calc totalMonths, totalAmt, changeAmt and averageChange
+    row_num = 0
 
     for row in csvreader:
+            # Read in the row value into variables 
             months.append(row[0])
-            budgetPerMonth.append(int(row[1]))
+            budget_per_month.append(int(row[1]))
+        
+            #determine the greatest increase and decrease amounts 
+            if row_num > 1:
+                change_amt = budget_per_month[row_num-1] - previous_amt
+
+                if (change_amt >= 0) and (change_amt > greatest_increase_amt):
+                    greatest_increase_amt = change_amt
+                    greatest_increase_month = months[row_num-1]
+                    print(f"greatest_increase_month {greatest_increase_month}")
+                elif (change_amt < 0) and (change_amt < greatest_decrease_amt):
+                    greatest_decrease_amt = change_amt
+                    greatest_decrease_month = months[row_num-1]
+                    print(f"greatest_decrease_month {greatest_decrease_month}")
+
+                previous_amt = budget_per_month[row_num-1]
+                print(f"previous_amt {previous_amt}")
+            else:
+                # sthere is no comparison needed for the first row
+                previous_amt = budget_per_month[row_num-1]
+                print(f"previous_amt {previous_amt}")
+
+
             # Calculate the total number of months included in the dataset
-            totalMonths = totalMonths + 1
-            print(totalMonths)
+            total_months = total_months + 1
+            
             # Calculate the net total amount of "Profit/Losses" over the entire period
-            totalAmt = totalAmt + (int(row[1]))
+            total_amt = total_amt + (int(row[1]))
 
-            # Put the budget profit-loss field into a list for processing later
-           # If int(totalMonths) > 2:
-                #previousAmt = budgetPerMonth[0]
-                #PLAmt = budgetPerMonth.[totalMonths] - previousAmt
-
-            #if row[1] == previousAmt: 
-            #print(int(row[1]).next())
-            #PLAmt.append((row[1]))
-            #print(PLAmt)
-            #print(previousAmt)
-
-            #changeAmt.append(rowChange)          
+            row_num = row_num + 1
 
 
-print(str(budgetPerMonth[5]))
 
 # Calc Average Change
-averageChange = totalAmt/totalMonths
+average_change = total_amt/total_months
 
 # The greatest increase in profits (date and amount) over the entire period
 # The greatest decrease in profits (date and amount) over the entire period
@@ -60,11 +78,11 @@ averageChange = totalAmt/totalMonths
 print("\n")
 print("Financial Analysis")
 print("----------------------------")
-print(f"Total Months: {totalMonths}")
-print(f"Total: {totalAmt}")
-print(f"Average  Change: {averageChange}")
-print(f"Greatest Increase in Profits:  ")
-print(f"Greatest Decrease in Profits:  ")
+print(f"Total Months: {total_months}")
+print(f"Total: $ {round(total_amt,2)}")
+print(f"Average  Change: $ {round(average_change,2)}")
+print(f"Greatest Increase in Profits: $ {greatest_increase_amt} {greatest_increase_month} ")
+print(f"Greatest Decrease in Profits: $ {greatest_decrease_amt} {greatest_decrease_month} ")
 print("\n")
 print("\n")
 
@@ -82,10 +100,10 @@ print("\n")
     # # Write the output to the file 
     # csvwriter.writerow(["Financial Analysis"])
     # csvwriter.writerow(["----------------------------"])
-    # csvwriter.writerow([Total Months: {totalMonths}")
-    # csvwriter.writerow(["Total: {totalAmt}")
-    # csvwriter.writerow(["Average  Change: {averageChange}")
-    # csvwriter.writerow(["Greatest Increase in Profits: Feb-2012 ($1926159) "])
-    # csvwriter.writerow(["Greatest Decrease in Profits: Sep-2013 ($-2196167) "])
+    # csvwriter.writerow([Total Months: {total_months}")
+    # csvwriter.writerow(["Total: {total_amt}")
+    # csvwriter.writerow(["Average  Change: {average_change}")
+    # csvwriter.writerow(["Greatest Increase in Profits: {greatest_increase_amt} "])
+    # csvwriter.writerow(["Greatest Decrease in Profits: {greatest_decrease_amt} "])
     
 
